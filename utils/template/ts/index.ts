0370f8676693676b2fff439f3b7b68f97f478a1e
Matthew Wrather <matt@wrathercreative.com>
2021-12-11 13:40:23 -0800

    update tmeplate

--- utils/template/ts/index.ts
+++ utils/template/ts/index.ts
@@ -1,8 +1,6 @@
 import getInput from '../../../utils/getInput';
 
 const data = getInput('${year}', '${day}');
-console.log(`Solution 1: ${part1(data)}`);
-console.log(`Solution 2: ${part2(data)}`);
 
 function part1(data: unknown) {
   return 'N/A';
@@ -11,3 +9,6 @@ function part1(data: unknown) {
 function part2(data: unknown) {
   return 'N/A';
 }
+
+console.log(`Solution 1: ${part1(data)}`);
+console.log(`Solution 2: ${part2(data)}`);
