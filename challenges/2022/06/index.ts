f3ffa88853a4893b7a05659c912c23dc6e9df3d6
Matthew Wrather <matt@wrathercreative.com>
2022-12-07 16:14:59 -0800

    2022-12-06 revision

--- challenges/2022/06/index.ts
+++ challenges/2022/06/index.ts
@@ -2,7 +2,7 @@ import getInput from '../../../utils/getInput';
 
 const data = getInput('2022', '06');
 
-const detectUniqueSubstring = (size: number, s: string): number => {
+const detectUniqueSubstring = (size: number) => (s: string): number => {
   for (let i = size; i < s.length; i += 1) {
     const substring = s.slice(i - size, i);
     if (new Set([...substring]).size === size) return i;
@@ -10,13 +10,9 @@ const detectUniqueSubstring = (size: number, s: string): number => {
   return 0;
 }
 
-function part1(data: string): number {
-  return detectUniqueSubstring(4, data);
-}
+const part1 = detectUniqueSubstring(4);
 
-function part2(data: string): number {
-  return detectUniqueSubstring(14, data);
-}
+const part2 = detectUniqueSubstring(14);
 
 console.log(`Solution 1: ${part1(data)}`);
 console.log(`Solution 2: ${part2(data)}`);
