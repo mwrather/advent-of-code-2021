dd50b90ed8a554b2263a31d5452bd81de83cb2d5
Matthew Wrather <matt@ofcolor.com>
2023-12-02 11:58:39 -0800

    refactor 2023-12-02

--- challenges/2023/02/index.ts
+++ challenges/2023/02/index.ts
@@ -1,7 +1,4 @@
-import * as R from 'ramda';
-
 import getInput from '../../../utils/getInput';
-import { match } from 'assert';
 
 const data = getInput('2023', '02');
 
@@ -9,42 +6,58 @@ const testInput = `Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
 Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
 Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
 Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
-Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green;`
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green;`;
 
-const process = (input: string) => {
-  return input.split('\n').map((line) => {
-    const matchResult = line.match(/^Game (\d+): (.*);?$/)
+const process = (
+  input: string
+): [
+  string,
+  {
+    reds: number;
+    greens: number;
+    blues: number;
+  }
+][] =>
+  input
+    .split('\n')
+    .map((line) => {
+      const matchResult = line.match(/^Game (\d+): (.*);?$/);
 
-    if (matchResult === null) {
-      throw new Error("Couldn't parse input");
-    }
+      if (matchResult === null) {
+        throw new Error("Couldn't parse input");
+      }
 
-    return matchResult.slice(1,3);
-  });
-}
+      return matchResult.slice(1, 3);
+    })
+    .map(([index, results]) => [
+      index,
+      {
+        reds: Math.max(
+          ...(results.match(/\d+(?= red)/g)?.map(Number) as number[])
+        ),
+        greens: Math.max(
+          ...(results.match(/\d+(?= green)/g)?.map(Number) as number[])
+        ),
+        blues: Math.max(
+          ...(results.match(/\d+(?= blue)/g)?.map(Number) as number[])
+        ),
+      },
+    ]);
 
-//
 function part1(data: string) {
-  return process(data).filter(([, results]) => {
-    const reds = results.match(/\d+(?= red)/g);
-    const greens = results.match(/\d+(?= green)/g);
-    const blues = results.match(/\d+(?= blue)/g);
-
-
-    return reds?.every(r => +r <= 12) && greens?.every(g => +g <= 13) && blues?.every(b => +b <= 14);
-  })
-  .reduce<number>((acc, [index]) => acc + +index, 0);
+  return process(data)
+    .filter(
+      ([, { reds, greens, blues }]) => reds <= 12 && greens <= 13 && blues <= 14
+    )
+    .reduce<number>((acc, [index]) => acc + +index, 0);
 }
 
 function part2(data: string) {
-  return process(data).map(([, results]) =>
-    [
-      results.match(/\d+(?= red)/g)?.map(Number) as number[],
-      results.match(/\d+(?= green)/g)?.map(Number) as number[],
-      results.match(/\d+(?= blue)/g)?.map(Number) as number[]
-    ].map<number>(ns => Math.max(...ns)))
-    .map(ns => ns.reduce((acc, curr) => acc * curr, 1))
-  .reduce<number>((acc, n) => acc + n, 0);
+  return process(data)
+    .map(([, results]) =>
+      Object.values(results).reduce<number>((acc, curr) => acc * curr, 1)
+    )
+    .reduce<number>((acc, n) => acc + n, 0);
 }
 
 console.assert(part1(testInput) === 8);
