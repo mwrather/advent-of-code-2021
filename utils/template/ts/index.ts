190d00bc710eb686cdcd9ca54f83a07678c8d1e6
Matthew Wrather <matt@ofcolor.com>
2023-12-27 16:41:54 -0800

    update template

--- utils/template/ts/index.ts
+++ utils/template/ts/index.ts
@@ -1,14 +1,22 @@
-import getInput from '../../../utils/getInput';
+import { readFileSync } from 'fs';
 
-const data = getInput('${year}', '${day}');
+const data = readFileSync(
+  'challenges/${year}/${day}/input.txt',
+  'utf-8'
+).trim();
 
-function part1(data: unknown) {
-  return 'N/A';
+const testData = ``;
+
+function part1(input: string) {
+  return 0;
 }
 
-function part2(data: unknown) {
-  return 'N/A';
+function part2(input: string) {
+  return 0;
 }
+
+console.assert(part1(testData) === 0);
+console.assert(part2(testData) === 0);
 
 console.log(`Solution 1: ${part1(data)}`);
 console.log(`Solution 2: ${part2(data)}`);
